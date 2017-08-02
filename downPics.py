a4dd64f538c3403bf5851800ae0ec6e1554970fa
Nikolay Mladenov <nidenov@gmail.com>
2017-08-02 11:15:03 +0200

    put code that worked where the code that didn't used to be

--- downPics.py
+++ downPics.py
@@ -13,25 +13,37 @@ folderForFolder.close()
 
 fileCurrent = open(folder + "current_condition")
 fileHourly = open(folder + "hourly_forecast")
+fileForecast = open(folder + "forecast")
 
 current_condition = fileCurrent.read()
 hourly_forecast = fileHourly.read();
+forecast = fileForecast.read();
 
 fileCurrent.close()
 fileHourly.close()
+fileForecast.close()
 
 
-
-#current condition
-values = json.loads(current_condition)
-
-current=values["current_observation"]
 
 
 print("Downloading weather pictures...")
+
+#current condition
+values = json.loads(current_condition)
+current = values["current_observation"]
+
 #download image
 pic = urllib.request.urlretrieve("http://icons.wxug.com/i/c/k/" + current["icon"] +".gif",  folder + "currentPic.gif")
 
+
+
+#forecast
+values = json.loads(forecast)
+current = values["forecast"]["txt_forecast"]["forecastday"][0]
+
+
+#download image
+pic = urllib.request.urlretrieve("http://icons.wxug.com/i/c/k/" + current["icon"] +".gif",  folder + "forecast.gif")
 
 
 
